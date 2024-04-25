88195b1cbbcca7dce4de1660afde0d138c50d8e6
honta kensuke <jianzibenduo0721@gmail.com>
2024-04-25 16:06:50 +0900

    スタイル、CSSのあて方

--- react-lesson/src/App.jsx
+++ react-lesson/src/App.jsx
@@ -1,8 +1,17 @@
+ // コンポーネントファイルは拡張子をjsxにする
+ 
  export const App = () => {
+  const contentButton = () => alert();
+  const fontStyle = {
+    color :"blue",
+    fontSize : "100px"
+  }
   return  (
     <>
-      <h1>こんにちは!</h1>
+      <h1 style = {fontStyle}>こんにちは!</h1>
       <p>お元気ですか？</p>
+      {/* {}で括ってあげればそこがJSとして読み込まれる */}
+      <button onClick={contentButton}>ボタン</button>
     </>
   )
 };
