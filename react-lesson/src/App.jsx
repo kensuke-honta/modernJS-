b054b7c9c7dc62813f6e1f1e4ca1524f87f904ef
honta kensuke <jianzibenduo0721@gmail.com>
2024-05-01 09:26:15 +0900

    state学習

--- react-lesson/src/App.jsx
+++ react-lesson/src/App.jsx
@@ -1,8 +1,12 @@
  // コンポーネントファイルは拡張子をjsxにする
+import { useState } from "react";
 import { ColorfulMessage } from "./components/colorfulMessage";
  
  export const App = () => {
-  const contentButton = () => alert();
+  const [num,setNum] = useState(0);//()の中に値を書くことで初期値に設定できる
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
   const fontStyle = {
     color :"blue",
     fontSize : "100px"
@@ -10,11 +14,13 @@ import { ColorfulMessage } from "./components/colorfulMessage";
   return  (
     <>
       <h1 style = {fontStyle}>こんにちは!</h1>
-      <ColorfulMessage color= "green">うるせえ！</ColorfulMessage>
-      <ColorfulMessage color = "red">こんちくしょー！</ColorfulMessage>
+      <ColorfulMessage color= "green">初めまして</ColorfulMessage>
+      <ColorfulMessage color = "red">こんにちは！</ColorfulMessage>
       <p>お元気です！</p>
       {/* {}で括ってあげればそこがJSとして読み込まれる */}
-      <button onClick={contentButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{ num }</p>
+      {/* {console.log(num)} */}
     </>
   )
 };
